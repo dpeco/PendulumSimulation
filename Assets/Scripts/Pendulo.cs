4abb6ac6f84b40d85ad7595e284eaf1a379c4de9
dpeco <yoshimcreary@hotmail.com>
2017-12-05 20:28:16 +0100

    ss

---
+++ Assets/Scripts/Pendulo.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulo : MonoBehaviour {
+
+    public Transform pos1;
+    public Transform pos2;
+
+    public float gravity;
+    public float cDrag;
+    public float initAngularVelX;
+    public float initAngleX;
+
+    public float initAngularVelZ;
+    public float initAngleZ;
+
+    private float curAngularVelX;
+    public float curAngleX;
+
+    private float curAngularVelZ;
+    private float curAngleZ;
+
+    public float stringDistance;
+
+    private float time = 0; //Tiempo total de ejecución
+
+    public bool method;
+    // Use this for initialization
+    void Start () {
+        stringDistance = Vector2.Distance(pos1.position, pos2.position);
+
+        initAngleX = pos1.transform.eulerAngles.x;
+
+        initAngleZ = pos1.transform.eulerAngles.z;
+
+        if (initAngleX > 180) initAngleX -= 360;
+        if (initAngleZ < -180) initAngleZ += 360;
+
+        curAngleX = initAngleX * Mathf.Deg2Rad;
+        curAngularVelX = initAngularVelX;
+
+        curAngleZ = initAngleZ * Mathf.Deg2Rad;;
+        curAngularVelZ = initAngularVelZ;
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        time += Time.deltaTime;
+
+        if (!method)
+        {
+            //FORMULA CALCULO DE ANGULO:
+            //PART1
+            float formulaPart1X;
+
+            formulaPart1X = initAngularVelX / Mathf.Sqrt(gravity / stringDistance) * Mathf.Sin(Mathf.Sqrt(gravity / stringDistance) * time);
+            //PART2
+            float formulaPart2X;
+
+            formulaPart2X = initAngleX * Mathf.Cos(Mathf.Sqrt(gravity / stringDistance) * time);
+
+            float newAngleX = formulaPart1X + formulaPart2X;
+
+            //---------
+
+            //FORMULA CALCULO DE ANGULO:
+            //PART1
+            float formulaPart1Z;
+
+            formulaPart1Z = initAngularVelZ / Mathf.Sqrt(gravity / stringDistance) * Mathf.Sin(Mathf.Sqrt(gravity / stringDistance) * time);
+            //PART2
+            float formulaPart2Z;
+
+            formulaPart2Z = initAngleZ * Mathf.Cos(Mathf.Sqrt(gravity / stringDistance) * time);
+
+            float newAngleZ = formulaPart1Z + formulaPart2Z;
+
+            //---------
+
+            pos1.transform.localEulerAngles = new Vector3(newAngleX, 0, newAngleZ);
+        }
+        else
+        {
+            //Mathf.Sin(curAngleX * Mathf.Rad2Deg)
+            float gl = gravity / stringDistance;
+            float drag;
+            //calculo numerico
+            drag = cDrag / stringDistance * curAngularVelX;
+            curAngularVelX += Time.deltaTime * ((-1 * gl * Mathf.Sin(curAngleX)) - drag);
+            curAngleX += Time.deltaTime * curAngularVelX;
+
+            drag = cDrag / stringDistance * curAngularVelZ;
+            curAngularVelZ += Time.deltaTime * ((-1 * gl * Mathf.Sin(curAngleZ)) - drag);
+            curAngleZ += Time.deltaTime * curAngularVelZ;
+
+            if (curAngleX > Mathf.PI) curAngleX -= 2 * Mathf.PI;
+            if (curAngleX < -Mathf.PI) curAngleX += 2 * Mathf.PI;
+
+            if (curAngleZ > Mathf.PI) curAngleZ -= 2 * Mathf.PI;
+            if (curAngleZ < -Mathf.PI) curAngleZ += 2 * Mathf.PI;
+
+            pos1.transform.localEulerAngles = new Vector3(curAngleX * Mathf.Rad2Deg, 0, curAngleZ * Mathf.Rad2Deg);
+        }
+    }
+}
---
+++ Assets/Scripts/QuaternionClass.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaternionClass
+{
+    public float w;
+    public float x;
+    public float y;
+    public float z;
+    // Use this for initialization
+    public QuaternionClass()
+    {
+        w = 0;
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+    public QuaternionClass(float a, float b, float c, float d)
+    {
+        w = a;
+        x = b;
+        y = c;
+        z = d;
+    }
+
+    QuaternionClass multiply(QuaternionClass q1, QuaternionClass q2)
+    {
+        QuaternionClass q3 = new QuaternionClass();
+        q3.w = q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z;
+        q3.x = q1.w * q2.x + q1.x * q2.w - q1.y * q2.z + q1.z * q2.y;
+        q3.y = q1.w * q2.y + q1.x * q2.z + q1.y * q2.w - q1.z * q2.x;
+        q3.z = q1.w * q2.z - q1.x * q2.y + q1.y * q2.x - q1.z * q2.w;
+
+        return q3;
+    }
+    QuaternionClass inverse(QuaternionClass q1)
+    {
+        QuaternionClass q2 = new QuaternionClass();
+
+        //transpose
+        q2.w = q1.w;
+        q2.x = -q1.x;
+        q2.y = -q1.y;
+        q2.z = -q1.z;
+        return multiply(q1, q2);
+    }
+    void convertFromAxisAngle(Vector3 axis, float a)
+    {
+
+        a = a / 360 * (float)Mathf.PI * 2;
+        w = Mathf.Cos(a / 2); //si w es menor que 0, l'angle es major que pi (LUL)
+
+        x = axis.x * Mathf.Sin(a / 2);
+        y = axis.y * Mathf.Sin(a / 2);
+        z = axis.z * Mathf.Sin(a / 2);
+
+    }
+    void convertToAxisAngle(QuaternionClass q1)
+    {
+        float a;
+        Vector3 axis;
+
+        a = 2 * Mathf.Acos(q1.w);
+        axis.x = q1.x / Mathf.Sqrt(1 - Mathf.Pow(q1.w, 2));
+        axis.y = q1.y / Mathf.Sqrt(1 - Mathf.Pow(q1.w, 2));
+        axis.z = q1.z / Mathf.Sqrt(1 - Mathf.Pow(q1.w, 2));
+
+    }
+    Vector3 rotation()
+    {
+        return new Vector3(x, y, z);
+    }
+}
